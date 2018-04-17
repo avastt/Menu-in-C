1451d974b81558e6165a0b7e24cf6ffe1d9b9372
Igor Frey <igorfreyy97@gmail.com>
2018-04-17 12:30:36 -0300

    Improved user search v0.2

--- Cmenu.cpp
+++ Cmenu.cpp
@@ -86,6 +86,7 @@ int pesquisar_pessoa(){
 				else
 				{
 					system("cls");
+					break;
 				}
 			}
 			teste = teste + 1;
@@ -103,6 +104,8 @@ int pesquisar_pessoa(){
 					else
 					{
 						system("cls");
+						break;
+						
 					}
 			}
 		}
